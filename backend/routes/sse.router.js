6173083cdf88388f861113b9e347ceca6b278900
Alessio Chessa <ale.chessa28@gmail.com>
2023-12-16 21:27:19 +0100

    fix: add temp fix to init sse api call

--- backend/routes/sse.router.js
+++ backend/routes/sse.router.js
@@ -3,6 +3,7 @@ import { init } from "../sse.js";
 
 const sseRouter = express.Router();
 
-sseRouter.get("/stream", init);
+//? idk what it's supposed to do but I have to respond to the request in some way
+sseRouter.get("/stream", (req, res) => { init(); res.send("init sse done"); }); // TODO: do something better here
 
 export default sseRouter;
